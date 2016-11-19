d6d41c9b531ce3415c3a50f4f041f5ac73e817f1
Joey Babcock <joeybab3@users.noreply.github.com>
2016-11-18 23:30:28 -0800

    added tip box for later

--- examples/Sonos_Controller_ESP8266/Sonos_Controller_ESP8266.ino
+++ examples/Sonos_Controller_ESP8266/Sonos_Controller_ESP8266.ino
@@ -425,8 +425,9 @@ void handleRoot() {
   msg += "<input type=\"range\" class=\"slider\"  min=\"0\" max=\"99\" value=\""+String(vol)+"\" name=\"volume-slider\" id=\"volume-slider\" onchange=\"setVolume(this.value)\" />\n";
   msg += "<p>Server Response:<div id=\"response\" class=\"response\"></div></p>\n";
   msg += "<p><form action=\"/\" method=\"get\" id=\"console\"><input placeholder=\"Enter a command...\" type=\"text\" id='console_text'/></form></p>\n";
-  msg += "<script>var intervalID = window.setInterval(getVolume, 5000);$('#console').submit(function(){parseCmd($(\"#console_text\").val());return false;});</script>";
+  msg += "<script>var intervalID = window.setInterval(getVolume, 5000);\n$('#console').submit(function(){parseCmd($(\"#console_text\").val());\nreturn false;\n});\n</script>\n";
   msg += "</div>\n";
+  msg += "<div id=\"tips\"></div>\n";
   msg == "</body>\n";
   msg += "</html>\n";
   server.send(200, "text/html", msg);

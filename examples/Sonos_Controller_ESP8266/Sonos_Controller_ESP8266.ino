5c2c224463119791964d87a4dfd9410f6afc8663
Joey Babcock <joeybab3@users.noreply.github.com>
2016-11-18 23:11:37 -0800

    added command send input

--- examples/Sonos_Controller_ESP8266/Sonos_Controller_ESP8266.ino
+++ examples/Sonos_Controller_ESP8266/Sonos_Controller_ESP8266.ino
@@ -92,8 +92,8 @@ WiFiClient client;
 SonosUPnP g_sonos = SonosUPnP(client, ethConnectError);
 
 // Living room
-IPAddress g_sonosLivingrIP(192, 168, 1, 245);
-const char g_sonosLivingrID[] = "5CAAFD406B62C";
+IPAddress g_JoeyIP(192, 168, 1, 204);
+const char g_JoeyID[] = "5CAAFD406B62C";
 // Bathroom
 IPAddress g_sonosBathroomIP(192, 168, 0, 202);
 const char g_sonosBathroomID[] = "000222222222";
@@ -210,14 +210,14 @@ String handleGet(String cmd)
     Serial.println("Handling command " + cmd);
     if (cmd == "gv")
     {
-      int volume = g_sonos.getVolume(g_sonosLivingrIP);
+      int volume = g_sonos.getVolume(g_JoeyIP);
       return String(volume);
     }
     else if (cmd == "gs")
     {
       String response = "";
       char uri[25] = "";
-      TrackInfo track = g_sonos.getTrackInfo(g_sonosLivingrIP, uri, sizeof(uri));
+      TrackInfo track = g_sonos.getTrackInfo(g_JoeyIP, uri, sizeof(uri));
       byte source = g_sonos.getSourceFromURI(track.uri);
       switch (source)
       {
@@ -264,59 +264,59 @@ void handleInput(String cmd, byte b1, byte b2)
     Serial.println("Handling command " + cmd);
     if (cmd == "pl")
     {
-      g_sonos.play(g_sonosLivingrIP);
+      g_sonos.play(g_JoeyIP);
     }
     // Pause
     else if (isCommand("pa", b1, b2))
     {
-      g_sonos.pause(g_sonosLivingrIP);
+      g_sonos.pause(g_JoeyIP);
     }
     // Stop
     else if (isCommand("st", b1, b2))
     {
-      g_sonos.stop(g_sonosLivingrIP);
+      g_sonos.stop(g_JoeyIP);
     }
     // Previous
     else if (isCommand("pr", b1, b2))
     {
-      g_sonos.skip(g_sonosLivingrIP, SONOS_DIRECTION_BACKWARD);
+      g_sonos.skip(g_JoeyIP, SONOS_DIRECTION_BACKWARD);
     }
     // Next
     else if (isCommand("nx", b1, b2))
     {
-      g_sonos.skip(g_sonosLivingrIP, SONOS_DIRECTION_FORWARD);
+      g_sonos.skip(g_JoeyIP, SONOS_DIRECTION_FORWARD);
     }
     // Play File
     else if (isCommand("fi", b1, b2))
     {
-      g_sonos.playFile(g_sonosLivingrIP, "192.168.188.22/Music/ringtone/ring1.mp3");
+      g_sonos.playFile(g_JoeyIP, "192.168.188.22/Music/ringtone/ring1.mp3");
 
     }
     // Play HTTP
     else if (isCommand("ht", b1, b2))
     {
       // Playing file from music service WIMP (SID = 20)
-      //g_sonos.playHttp(g_sonosLivingrIP, "trackid_37554547.mp4?sid=20&amp;flags=32");
+      //g_sonos.playHttp(g_JoeyIP, "trackid_37554547.mp4?sid=20&amp;flags=32");
 
-      //g_sonos.playHttp(g_sonosLivingrIP, "http://192.168.188.1:49200/AUDIO/DLNA-1-0/MXT-USB-StorageDevice-01/take_me_to_church.mp3");
-      g_sonos.playHttp(g_sonosLivingrIP, "http://192.168.188.28:88/gong.mp3");
+      //g_sonos.playHttp(g_JoeyIP, "http://192.168.188.1:49200/AUDIO/DLNA-1-0/MXT-USB-StorageDevice-01/take_me_to_church.mp3");
+      g_sonos.playHttp(g_JoeyIP, "http://192.168.188.28:88/gong.mp3");
     }
     // Play Radio
     else if (isCommand("ra", b1, b2))
     {
-      g_sonos.playRadio(g_sonosLivingrIP, "//lyd.nrk.no/nrk_radio_p3_mp3_h.m3u", "NRK P3");
+      g_sonos.playRadio(g_JoeyIP, "//lyd.nrk.no/nrk_radio_p3_mp3_h.m3u", "NRK P3");
 
     }
     // Play Line In
     else if (isCommand("li", b1, b2))
     {
-      g_sonos.playLineIn(g_sonosLivingrIP, g_sonosLivingrID);
+      g_sonos.playLineIn(g_JoeyIP, g_JoeyID);
     }
     // Group
     else if (isCommand("gr", b1, b2))
     {
-      g_sonos.playConnectToMaster(g_sonosBedroomIP, g_sonosLivingrID);
-      g_sonos.playConnectToMaster(g_sonosBathroomIP, g_sonosLivingrID);
+      g_sonos.playConnectToMaster(g_sonosBedroomIP, g_JoeyID);
+      g_sonos.playConnectToMaster(g_sonosBathroomIP, g_JoeyID);
     }
     // UnGroup
     else if (isCommand("ug", b1, b2))
@@ -327,42 +327,42 @@ void handleInput(String cmd, byte b1, byte b2)
     // Repeat On
     else if (isCommand("re", b1, b2))
     {
-      g_sonos.setPlayMode(g_sonosLivingrIP, SONOS_PLAY_MODE_REPEAT);
+      g_sonos.setPlayMode(g_JoeyIP, SONOS_PLAY_MODE_REPEAT);
     }
     // Shuffle On
     else if (isCommand("sh", b1, b2))
     {
-      g_sonos.setPlayMode(g_sonosLivingrIP, SONOS_PLAY_MODE_SHUFFLE);
+      g_sonos.setPlayMode(g_JoeyIP, SONOS_PLAY_MODE_SHUFFLE);
     }
     // Repeat and Shuffle On
     else if (isCommand("rs", b1, b2))
     {
-      g_sonos.setPlayMode(g_sonosLivingrIP, SONOS_PLAY_MODE_SHUFFLE_REPEAT);
+      g_sonos.setPlayMode(g_JoeyIP, SONOS_PLAY_MODE_SHUFFLE_REPEAT);
     }
     // Repeat and Shuffle Off
     else if (isCommand("no", b1, b2))
     {
-      g_sonos.setPlayMode(g_sonosLivingrIP, SONOS_PLAY_MODE_NORMAL);
+      g_sonos.setPlayMode(g_JoeyIP, SONOS_PLAY_MODE_NORMAL);
     }
     // Loudness On
     else if (isCommand("lo", b1, b2))
     {
-      g_sonos.setLoudness(g_sonosLivingrIP, true);
+      g_sonos.setLoudness(g_JoeyIP, true);
     }
     // Loudness Off
     else if (isCommand("l_", b1, b2))
     {
-      g_sonos.setLoudness(g_sonosLivingrIP, false);
+      g_sonos.setLoudness(g_JoeyIP, false);
     }
     // Mute On
     else if (isCommand("mu", b1, b2))
     {
-      g_sonos.setMute(g_sonosLivingrIP, true);
+      g_sonos.setMute(g_JoeyIP, true);
     }
     // Mute Off
     else if (isCommand("m_", b1, b2))
     {
-      g_sonos.setMute(g_sonosLivingrIP, false);
+      g_sonos.setMute(g_JoeyIP, false);
     }
     // Volume/Bass/Treble
     else if (b2 >= '0' && b2 <= '9')
@@ -370,34 +370,34 @@ void handleInput(String cmd, byte b1, byte b2)
       // Volume 0 to 99
       if (b1 >= '0' && b1 <= '9')
       {
-        g_sonos.setVolume(g_sonosLivingrIP, ((b1 - '0') * 10) + (b2 - '0'));
+        g_sonos.setVolume(g_JoeyIP, ((b1 - '0') * 10) + (b2 - '0'));
       }
       // Bass 0 to -9
       else if (b1 == 'b')
       {
-        g_sonos.setBass(g_sonosLivingrIP, (b2 - '0') * -1);
+        g_sonos.setBass(g_JoeyIP, (b2 - '0') * -1);
       }
       // Bass 0 to 9
       else if (b1 == 'B')
       {
-        g_sonos.setBass(g_sonosLivingrIP, b2 - '0');
+        g_sonos.setBass(g_JoeyIP, b2 - '0');
       }
       // Treble 0 to -9
       else if (b1 == 't')
       {
-        g_sonos.setTreble(g_sonosLivingrIP, (b2 - '0') * -1);
+        g_sonos.setTreble(g_JoeyIP, (b2 - '0') * -1);
       }
       // Treble 0 to 9
       else if (b1 == 'T')
       {
-        g_sonos.setTreble(g_sonosLivingrIP, b2 - '0');
+        g_sonos.setTreble(g_JoeyIP, b2 - '0');
       }
     }
 
     else if (isCommand("ti", b1, b2))
     {
       Serial.println("we want the track uri");
-      TrackInfo track = g_sonos.getTrackInfo(g_sonosLivingrIP, uri, sizeof(uri));
+      TrackInfo track = g_sonos.getTrackInfo(g_JoeyIP, uri, sizeof(uri));
       Serial.println(uri);
     }
 
@@ -406,7 +406,7 @@ void handleInput(String cmd, byte b1, byte b2)
 /* WebServer Stuff */
 
 void handleRoot() {
-  int vol = g_sonos.getVolume(g_sonosLivingrIP);
+  int vol = g_sonos.getVolume(g_JoeyIP);
   String msg = "<html>\n";
   msg += "<head>\n";
   msg += "<title>ESP8266 Sonos Controller</title>\n";
@@ -424,7 +424,8 @@ void handleRoot() {
   msg += "<h3>Volume: <span id=\"vol\">"+String(vol)+"</span><input type=\"hidden\" id='volume' value='"+String(vol)+"' onchange=\"setVolume(this.value)\"/></h3><br/>\n";
   msg += "<input type=\"range\" class=\"slider\"  min=\"0\" max=\"99\" value=\""+String(vol)+"\" name=\"volume-slider\" id=\"volume-slider\" onchange=\"setVolume(this.value)\" />\n";
   msg += "<p>Server Response:<div id=\"response\" class=\"response\"></div></p>\n";
-  msg += "<script>var intervalID = window.setInterval(getVolume, 3000);var intervalID = window.setInterval(getTrack, 10000);</script>";
+  msg += "<p><form action=\"/\" method=\"get\" id=\"console\"><input placeholder=\"Enter a command...\" type=\"text\" id='console_text'/></form></p>\n";
+  msg += "<script>var intervalID = window.setInterval(getVolume, 5000);$('#console').submit(function(){parseCmd($(\"#console_text\").val());return false;});</script>";
   msg += "</div>\n";
   msg == "</body>\n";
   msg += "</html>\n";
